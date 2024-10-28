c4108e149f48fe7681ed17298c919b4b30df1f16
jotavee-dev <joaovitorengcomp@edu.unifor.br>
2024-10-28 13:51:09 -0300

    Add initial project files and configurations

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+export default function App() {
+  return (
+    <>
+    <h1>Pagina App</h1>
+    {/* <Logo></Logo>
+    <Nav></Nav>
+    <Header></Header>
+    <Main></Main>
+    <Section></Section>
+    <Footer></Footer> */}
+    </>
+  );
+}
