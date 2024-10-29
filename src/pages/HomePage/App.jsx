6e8ea59cbfdbb8a4aed6cf5385ec42a410b26005
tayara kerlen <tayara.kerlen1@gmail.com>
2024-10-29 15:52:13 -0300

    tayara

--- src/pages/HomePage/App.jsx
+++ src/pages/HomePage/App.jsx
@@ -1,3 +1,4 @@
+import Busca from "../../components/busca";
 import Layout from "../../components/Layout/App";
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <Layout>
       
     </Layout>
+    <Busca></Busca>
     </>
   );
 }
