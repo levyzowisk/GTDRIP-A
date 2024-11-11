feb41af9740aed50bea330363fe30d894fb772d7
jotavee-dev <joaovitorengcomp@edu.unifor.br>
2024-11-11 01:42:50 -0300

    Adiciona contêiner para a página de visualização de produtos, ajusta estilos e organiza layout entre Gallery e BuyBox

--- src/pages/ProductViewPage/App.jsx
+++ src/pages/ProductViewPage/App.jsx
@@ -2,7 +2,6 @@ import ProductListing from "../../components/ProductListing/App";
 import Gallery from "../../components/Gallery/app2";
 import Layout from "../../components/Layout/App";
 import Section from "../../components/Section/App";
-import ProductDetails from "../../components/ProductDetails/App";
 import "./style.css";
 import BuyBox from "../../components/BuyBox/App";
 import ProductOptions from "../../components/ProductOptions/App";
@@ -11,25 +10,27 @@ export default function ProductViewPage() {
   return (
     <>
       <Layout>
-        <Gallery></Gallery>
-        <BuyBox
-          name="Tênis Nike Air Max"
-          category="Esportivo | Nike | REF:12345"
-          stars={4.8}
-          reviewCount={200}
-          price="R$ 249,99"
-          originalPrice="R$ 350,00"
-          description="Tênis confortável para a prática de esportes."
-          sizeOptions={["37", "38", "39", "40", "41"]}
-          colorOptions={["#ff5c5c", "#000", "#00c3ff", "#ffff00"]}
-        >
-          <ProductOptions
-            options={["39", "40", "41"]}
-            radius="5px"
-            shape="square"
-            type="text"
-          />
-        </BuyBox>
+        <div className="product-view-container">
+          <Gallery />
+          <BuyBox
+            name="Tênis Nike Air Max"
+            category="Esportivo | Nike | REF:12345"
+            stars={4.8}
+            reviewCount={200}
+            price="R$ 249,99"
+            originalPrice="R$ 350,00"
+            description="Tênis confortável para a prática de esportes."
+            sizeOptions={["37", "38", "39", "40", "41"]}
+            colorOptions={["#ff5c5c", "#000", "#00c3ff", "#ffff00"]}
+          >
+            <ProductOptions
+              options={["39", "40", "41"]}
+              radius="5px"
+              shape="square"
+              type="text"
+            />
+          </BuyBox>
+        </div>
         <Section title="Produtos relacionados">
           <ProductListing />
         </Section>
@@ -37,7 +38,6 @@ export default function ProductViewPage() {
     </>
   );
 }
-<BuyBox />;
 
 // name="Tênis Nike Air Max"
 // reference="123456"
