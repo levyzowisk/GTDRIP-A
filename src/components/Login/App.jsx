e33322bc612696693c9a78127d40b1bc233dc36a
tayara kerlen <tayara.kerlen1@gmail.com>
2024-11-12 17:25:37 -0300

    login

---
+++ src/components/Login/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Acesse sua conta</h2>
+        <p>Novo cliente? Então registre-se <a href="/register">aqui</a>.</p>
+        <form>
+          <label>Login *</label>
+          <input type="text" placeholder="Insira seu login ou email" required />
+
+          <label>Senha *</label>
+          <input type="password" placeholder="Insira sua senha" required />
+
+          <a href="/forgot-password" className="forgot-password">Esqueci minha senha</a>
+
+          <button type="submit" className="login-button">Acessar Conta</button>
+        </form>
+        <p>Ou faça login com <button className="google-button">G</button> <button className="facebook-button">F</button></p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/Cadastro/App.jsx
+++ src/pages/Cadastro/App.jsx
@@ -3,12 +3,14 @@ import Gallery from "../../components/Gallery/app";
 import Layout from "../../components/Layout/App";
 import Section from "../../components/Section/App";
 import "./style.css";
+import Login from "../../components/Login/App";
 
 
 export default function Cadastro() {
   return (
     <>
       <Layout>
+        <Login></Login>
       </Layout>
     </>
   );
