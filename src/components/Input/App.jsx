581da68f07f62df590664c5070697ca39151316b
unknown <kawanmendes6214@digitalcollege.internal>
2024-11-14 13:38:32 -0300

    Construindo

---
+++ src/components/Input/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function Input (props) {
+    return (
+        <>
+
+        <div className="campo"> 
+         <form className="info"> <label htmlFor={props.htmlFor}>{props.text}</label>
+       <input id={props.id} type={props.type} placeholder={props.placeholder} />           
+         </form> </div>
+        </>
+    
+        )
+}
+
+
+export default Input;
--- src/pages/Cadastro/App.jsx
+++ src/pages/Cadastro/App.jsx
@@ -3,12 +3,81 @@ import Gallery from "../../components/Gallery/app";
 import Layout from "../../components/Layout/App";
 import Section from "../../components/Section/App";
 import "./style.css";
+import Input from "../../components/Input/App";
 
 
 export default function Cadastro() {
   return (
     <>
       <Layout>
+        
+        <Input
+            htmlFor ="nome"
+            text ="Nome completo *"
+            id = "nome"
+            type="text"
+            placeholder ="Insira seu Nome"
+        />
+        <Input
+            htmlFor ="email"
+            text ="E-mail *"
+            id = "email"
+            type="text"
+            placeholder ="Insira seu E-MAIL"
+        />
+        <Input
+            htmlFor ="cpf"
+            text ="CPF *"
+            id = "cpf"
+            type="text"
+            placeholder ="Insira seu CPF"
+        />
+          <Input
+            htmlFor ="celular"
+            text ="Celular *"
+            id = "celular"
+            type="text"
+            placeholder ="Insira seu Celular"
+        />
+        <Input
+            htmlFor ="endereço"
+            text ="Endereço *"
+            id = "endereço"
+            type="text"
+            placeholder ="Insira seu Endereço"
+        />
+        <Input
+            htmlFor ="bairro"
+            text ="Bairro *"
+            id = "bairro"
+            type="text"
+            placeholder ="Insira seu Bairro"
+        />
+         <Input
+            htmlFor ="cidade"
+            text ="Cidade *"
+            id = "cidade"
+            type="text"
+            placeholder ="Insira sua Cidade"
+        />
+         <Input
+            htmlFor ="Cep"
+            text ="CEP *"
+            id = "Cep"
+            type="text"
+            placeholder ="Insira sua CEP"
+        />
+        <Input
+            htmlFor ="complemento"
+            text ="Complemento *"
+            id = "complemento"
+            type="text"
+            placeholder ="Insira sua CEP"
+        />
+         
+        
+
+☻
       </Layout>
     </>
   );
